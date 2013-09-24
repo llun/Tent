193b894396a2de31bce9258e209769034395d04f
Maythee Anegboonlap <natkung@gmail.com>
2013-09-24 22:42:24 +0800

    Format topic code.

--- src/in/th/llun/tent/model/Topic.java
+++ src/in/th/llun/tent/model/Topic.java
@@ -12,37 +12,37 @@ import android.util.Log;
 public class Topic extends JSONRemoteObject {
 
 	public Topic(JSONObject raw) {
-	  super(raw);
-  }
-	
+		super(raw);
+	}
+
 	public String getId() {
 		return mRaw.optString("id");
 	}
-	
+
 	public String getTitle() {
 		return mRaw.optString("title");
 	}
-	
+
 	public String getExcerpt() {
 		return mRaw.optString("excerpt");
 	}
-	
+
 	public People getLastUpdater() {
 		return new People(mRaw.optJSONObject("last_updater"));
 	}
-	
+
 	public String getTopicId() {
 		return getTopicable().optString("id");
 	}
-	
+
 	public String getType() {
 		return getTopicable().optString("type");
 	}
-	
+
 	public String getUrl() {
 		return getTopicable().optString("url");
 	}
-	
+
 	public Date getUpdatedAt() {
 		try {
 			return Formatter.dateFromStringWithTimezone(mRaw.optString("updated_at"));
@@ -60,11 +60,11 @@ public class Topic extends JSONRemoteObject {
 			return null;
 		}
 	}
-	
+
 	public int getNumberOfAttachment() {
 		return mRaw.optInt("attachments");
 	}
-	
+
 	private JSONObject getTopicable() {
 		return mRaw.optJSONObject("topicable");
 	}
